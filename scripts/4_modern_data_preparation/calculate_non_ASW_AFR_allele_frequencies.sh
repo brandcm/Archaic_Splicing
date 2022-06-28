dc05b4b9e19ff6a16d70d9c515c138975d8a02de
Colin M. Brand <44248136+brandcm@users.noreply.github.com>
2022-06-28 11:38:06 -0700

    Add files via upload

---
+++ scripts/4_modern_data_preparation/calculate_non_ASW_AFR_allele_frequencies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -N calculate_non_ASW_AFR_allele_frequencies
+#$ -M colin.brand@ucsf.edu
+#$ -m ae
+#$ -cwd
+#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/4_modern_data_preparation/calculate_non_ASW_AFR_allele_frequencies.out
+#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/4_modern_data_preparation/calculate_non_ASW_AFR_allele_frequencies.err
+#$ -l h_rt=6:00:00
+#$ -l mem_free=20G
+
+# load modules
+module load CBI
+module load bcftools/1.13
+
+# change directories
+cd ../../data/archaic_variants_in_humans
+
+# get any archaic variants also present in 1000 Genomes, splice altering or not
+bcftools view  -S ../thousand_genomes/non_ASW_AFR_1000_genomes_samples.txt archaic_variants_subset_concat_normalized_1000_genomes.vcf.gz -O z -o archaic_variants_subset_concat_normalized_1000_genomes_non_ASW_AFR.vcf.gz
+bcftools index -t archaic_variants_subset_concat_normalized_1000_genomes_non_ASW_AFR.vcf.gz
+
+# query chrom, pos, ref allele, alt allele, allele count, allele number, and allele frequencies
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%AC\t%AN\n' archaic_variants_subset_concat_normalized_1000_genomes_non_ASW_AFR.vcf.gz > non_ASW_AFR_allele_frequencies_hg38.txt
+echo "query complete"
---
+++ scripts/4_modern_data_preparation/concat_sQTLs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -N concat_sQTLs
+#$ -M colin.brand@ucsf.edu
+#$ -m ae
+#$ -cwd
+#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/4_modern_data_preparation/concat_sQTLs.out
+#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/4_modern_data_preparation/concat_sQTLs.err
+#$ -l h_rt=1:00:00
+#$ -l mem_free=5G
+
+# load modules
+
+
+# run script
+cd ../../data/GTEx_sQTLs/signif_pairs
+for f in *.txt; do awk -F _ '{print $1"_"$2,$3,$4,ARGV[2]}' OFS='\t' $f > chrom_pos_$f & done
+wait
+
+for f in chrom_pos_*.txt; do sed 's/\..*//' $f | tail -n +2 > new_$f & done
+wait
+
+cat new_*.txt > concat_sQTLs.txt
+
+mv concat_sQTLs.txt ../
+
+rm chrom_pos_*.txt
+rm new_*.txt
