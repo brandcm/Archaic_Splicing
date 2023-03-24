34f13d032b853153724c1b35d7cfcd130cfd0c43
Colin M. Brand <44248136+brandcm@users.noreply.github.com>
2023-03-24 15:05:48 -0700

    Update query_gnomAD_allele_frequencies.sh

--- scripts/4_modern_data_preparation/query_gnomAD_allele_frequencies.sh
+++ scripts/4_modern_data_preparation/query_gnomAD_allele_frequencies.sh
@@ -16,7 +16,7 @@ module load bcftools/1.13
 cd ../../data/archaic_variants_in_humans/
 
 # set path
-gnomAD_vcfs=../../../../data/wynton_databases/gnomAD/3.0/variants/genomes
+gnomAD_vcfs="../../../../data/wynton_databases/gnomAD/3.0/variants/genomes"
 
 # set variable
 chrs=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8' 'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16' 'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' )
