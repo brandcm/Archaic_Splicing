f79cb275c80e3b8774f8d3ace00e3fc1b09c01e0
Colin M. Brand <44248136+brandcm@users.noreply.github.com>
2023-03-24 15:13:09 -0700

    Add files via upload

--- scripts/3_SpliceAI_analysis/concat_spliceai_indel_vcfs.sh
+++ scripts/3_SpliceAI_analysis/concat_spliceai_indel_vcfs.sh
@@ -3,8 +3,8 @@
 #$ -M colin.brand@ucsf.edu
 #$ -m ae
 #$ -cwd
-#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/concat_spliceai_indel_vcfs.out
-#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/concat_spliceai_indel_vcfs.err
+#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/3_spliceai_analysis/concat_spliceai_indel_vcfs.out
+#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/3_spliceai_analysis/concat_spliceai_indel_vcfs.err
 #$ -l h_rt=6:00:00
 #$ -l mem_free=20G
 
@@ -12,25 +12,24 @@
 module load CBI
 module load bcftools/1.13
 
+# change directories
+cd ../../data/spliceai_outputs
+
 # concatenate files
-#bcftools concat ../data/split.indel.a*.vcf -O z -o ../data/splice_annotated_archaic_indels_2.vcf.gz  
-#bcftools sort ../data/splice_annotated_archaic_indels_2.vcf.gz -O z -o ../data/splice_annotated_archaic_indels.vcf.gz
-#echo "concat complete"
-#rm ../data/splice_annotated_archaics_indels_2.vcf.gz
-#rm ../data/split.a*.vcf
-#rm ../data/split.a*.vcf.gz 
-#rm ../data/split.a*.vcf.gz.tbi
+bcftools concat split.indel.a*.vcf -O z -o splice_annotated_archaic_indels_2.vcf.gz  
+bcftools sort splice_annotated_archaic_indels_2.vcf.gz -O z -o splice_annotated_archaic_indels.vcf.gz
+echo "concat complete"
+
+# remove previous files
+rm splice_annotated_archaics_indels_2.vcf.gz
+rm split.indel.a*.vcf
+rm ../archaic_genomes/split.indel.a*.vcf.gz
 
 # index concantenated file
-#bcftools index -t ../data/splice_annotated_archaic_indels.vcf.gz
-#echo "indexing complete"
+bcftools index -t splice_annotated_archaic_indels.vcf.gz
+echo "indexing complete"
 
 # get relevant fields, remove everything in INFO that's not the alternate allele, turn pipes into tabs, turn commas into new lines, and fill in CHROM, POS, and REF for extra annotations
-bcftools query -f '%CHROM\t%POS\t%REF\t[%GT\t]%INFO\n' ../data/splice_annotated_archaic_indels.vcf.gz | awk '{sub(/.*SpliceAI=/, "", $7)}1' | tr '|' '\t' | tr ',' '\n' |
+bcftools query -f '%CHROM\t%POS\t%REF\t[%GT\t]%INFO\n' splice_annotated_archaic_indels.vcf.gz | awk '{sub(/.*SpliceAI=/, "", $7)}1' | tr '|' '\t' | tr ',' '\n' |
 awk 'NF==10{print p"\t"q"\t"r"\t"s"\t"t"\t"u"\t"$1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10; next} {p=$1}{q=$2}{r=$3}{s=$4}{t=$5}{u=$6} 1' |
-awk 'NF==16{print}{}' | tr ' ' '\t' > ../data/spliceai_archaic_indels.txt
-
-# delete error file if empty
-if [ ! -s concat_spliceai_indel_vcfs.err ] ; then
-  rm concat_spliceai_indel_vcfs.err
-fi
+awk 'NF==16{print}{}' | tr ' ' '\t' > spliceai_archaic_indels.txt
--- scripts/3_SpliceAI_analysis/spliceai_indel_array.sh
+++ scripts/3_SpliceAI_analysis/spliceai_indel_array.sh
@@ -4,16 +4,17 @@
 #$ -M colin.brand@ucsf.edu
 #$ -m ae
 #$ -cwd
-#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/spliceai_indel_array.out
-#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/spliceai_indel_array.err
+#$ -o ~/../../../group/capra/projects/archaic_splicing/scripts/3_spliceai_analysis/spliceai_indel_array.out
+#$ -e ~/../../../group/capra/projects/archaic_splicing/scripts/3_spliceai_analysis/spliceai_indel_array.err
 #$ -l h_rt=48:00:00
 #$ -l mem_free=20G
-
-# load modules
-
 
 # load conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate spliceai
 
-spliceai -I ../data/split.indel.a$SGE_TASK_ID.vcf.gz -O ../data/split.indel.a$SGE_TASK_ID.vcf -R ../data/hg19.fa -A grch37
+# set path to hg19 and change directories
+hg19="../../../../data/hg19_fasta/2022-03-14/hg19.fa"
+cd ../../data/archaic_genomes
+
+spliceai -I split.indel.a"$SGE_TASK_ID".vcf.gz -O ../spliceai_outputs/split.indel.a"$SGE_TASK_ID".vcf -R "$hg19" -A grch37
