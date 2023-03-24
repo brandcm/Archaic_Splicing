f33377d5c8e4ba3bd1fd3017049348583645932c
Colin M. Brand <44248136+brandcm@users.noreply.github.com>
2023-03-24 15:08:54 -0700

    Update subset_1KG.sh

--- scripts/4_modern_data_preparation/subset_1KG.sh
+++ scripts/4_modern_data_preparation/subset_1KG.sh
@@ -12,16 +12,16 @@
 module load CBI
 module load bcftools/1.13
 
-# designate path to 1000 genomes data, hg19 fasta, and change directories
+# designate path to 1000 genomes data, hg38 fasta, and change directories
 thousand_genomes="../../../../data/wynton_databases/1000_genomes/release/20190312_biallelic_SNV_and_INDEL/"
-hg38_fasta="../../../../data/hg38_fasta/2022-03-14/hg38.fa"
+hg38="../../../../data/hg38_fasta/2022-03-14/hg38.fa"
 cd ../../data/thousand_genomes
 
 # list chromosomes
 chrs=('chr1' 'chr2' 'chr3' 'chr4' 'chr5' 'chr6' 'chr7' 'chr8' 'chr9' 'chr10' 'chr11' 'chr12' 'chr13' 'chr14' 'chr15' 'chr16' 'chr17' 'chr18' 'chr19' 'chr20' 'chr21' 'chr22' 'chrX' )
 
 # normalize 1000 genomes  
-for c in ${chrs[@]}; do bcftools norm -m - -f "$hg38_fasta" "$thousand_genomes"ALL."$c".shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz -O z -o normalized_"$c"_1000_genomes.vcf.gz & done
+for c in ${chrs[@]}; do bcftools norm -m - -f "$hg38" "$thousand_genomes"ALL."$c".shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz -O z -o normalized_"$c"_1000_genomes.vcf.gz & done
 wait
 echo "normalization complete"
 
